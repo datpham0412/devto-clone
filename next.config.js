af7a9e21bd31cfed8d7256ce4a8eb106cb8e9deb
datpham0412 <tiendat041202@gmail.com>
2024-10-28 10:00:28 +1100

    feat: Update next.config.js and package-lock.json

--- next.config.js
+++ next.config.js
@@ -2,12 +2,13 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import './src/env.js';
+import "./src/env.js";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: false,
   images: {
-    domains: ['picsum.photos', 'media2.dev.to'],
+    domains: ["picsum.photos", "media2.dev.to"],
   },
 };
 
