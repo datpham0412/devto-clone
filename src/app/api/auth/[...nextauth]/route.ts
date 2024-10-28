658ad3d9f1c2d9a70a960c8817e10e406e4a8aba
datpham0412 <tiendat041202@gmail.com>
2024-10-28 15:48:43 +1100

    feat: setup route for OAuth Provider

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,42 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
+
+const prisma = new PrismaClient();
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  callbacks: {
+    session: async ({ session, user }) => {
+      if (session?.user) {
+        session.user.id = user.id;
+        session.user.username = user.username;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: '/auth/signin',
+    signUp: '/auth/signup',
+  },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
