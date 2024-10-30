f45dccdb8c32de0b1255adb1f4de7da5e3a18c09
datpham0412 <tiendat041202@gmail.com>
2024-10-30 16:59:16 +1100

    fix: Remove callback endpoint

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,9 @@
-const production = process.env.NODE_ENV === 'production';
+const production = process.env.NODE_ENV === "production";
 
 export const config = {
-  appUrl: process.env.NEXT_PUBLIC_APP_URL ?? (production 
-    ? 'https://devto-clone-datpham.vercel.app' 
-    : 'http://localhost:3000'),
-  supabase: {
-    url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    anonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  },
-  auth: {
-    confirmEmailRedirect: '/auth/callback',
-  },
-}; 
+  appUrl:
+    process.env.NEXT_PUBLIC_APP_URL ??
+    (production
+      ? "https://devto-clone-datpham.vercel.app"
+      : "http://localhost:3000"),
+};
