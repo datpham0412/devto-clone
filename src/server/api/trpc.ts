9efa8bb9f07358ac10686ddec631d93019de6874
datpham0412 <tiendat041202@gmail.com>
2024-10-30 17:03:24 +1100

    feat: Inialize TRPC server side configuration instance

---
+++ src/server/api/trpc.ts
@@ -0,0 +1,37 @@
+import { initTRPC, TRPCError } from "@trpc/server";
+import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
+import superjson from "superjson";
+import { getServerSession } from "next-auth";
+import { prisma } from "~/lib/prisma";
+import { authOptions } from "~/lib/auth";
+
+export const createTRPCContext = async (opts: CreateNextContextOptions) => {
+  const session = await getServerSession(authOptions);
+
+  return {
+    session,
+    prisma,
+  };
+};
+
+const t = initTRPC.context<typeof createTRPCContext>().create({
+  transformer: superjson,
+  errorFormatter({ shape }) {
+    return shape;
+  },
+});
+
+export const createTRPCRouter = t.router;
+export const publicProcedure = t.procedure;
+
+export const protectedProcedure = t.procedure.use(async ({ ctx, next }) => {
+  if (!ctx.session?.user) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+  return next({
+    ctx: {
+      ...ctx,
+      session: { ...ctx.session, user: ctx.session.user },
+    },
+  });
+});
